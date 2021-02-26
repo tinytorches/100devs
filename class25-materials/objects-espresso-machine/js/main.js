26a611c3f747dc8349fb62a00d686cd284c53206
Tim Calas <timcalas@gmail.com>
2021-02-26 11:59:29 -0800

    made changes to in-class lab and created tic-tac-toe folder

--- class25-materials/objects-espresso-machine/js/main.js
+++ class25-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,30 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+// class EspressoMachine {
+//     constructor(espressoMachineBrand,espressoMachineModel, espressoMachineColor, espressoMachine, espressoMachinePrice) {
+//         this.brand = espressoMachineBrand
+//         this.model = espressoMachineModel
+//     }
+// }
+
+// CLASS SOLUTION
+class EspressoMachine {
+    constructor(color, make, model, price) {
+        this.color = color
+        this.make = make
+        this.model = model
+        this.price = price
+    }
+    start() {
+        console.log('Starting to make espresso')
+    }
+
+    strop() {
+        console.log('Turning off')
+    }
+
+    steam() {
+        console.log('Building steam')
+    }
+}
+
+let gaggia = new EspressoMachine('red', 'Gaggia', 'CLassic Pro', 400)
